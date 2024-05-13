abe68f9e4ab207bb272625877b4a6b3f8f447924
SmileNattakit <nattakit.rattanakeha@gmail.com>
2024-05-13 15:13:29 +0700

    finish task asignment

---
+++ script.js
@@ -0,0 +1,16 @@
+const dayNumber = 2; // input 1 - 7  โดย 1 เท่ากับวันอาทิตย์ 7 เท่ากับวันเสาร์
+if (dayNumber === 1) {
+  console.log("สวัสดีวันจันทร์");
+} else if (dayNumber === 2) {
+  console.log("สวัสดีอังคาร");
+} else if (dayNumber === 3) {
+  console.log("สวัสดีพุธ");
+} else if (dayNumber === 4) {
+  console.log("สวัสดีพฤหัสบดี");
+} else if (dayNumber === 5) {
+  console.log("สวัสดีศุกร์");
+} else if (dayNumber === 6) {
+  console.log("สวัสดีเสาร์");
+} else {
+  console.log("สวัสดีอาทิตย์");
+}
